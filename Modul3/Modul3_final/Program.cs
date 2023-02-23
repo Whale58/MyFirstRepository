7156d84b35192841781f8128e86155687a052b03
Whale <blagodatskih.vs@mail.ru>
2023-02-23 15:18:09 +0300

    Выполнено финальное задание модуля 3

---
+++ Modul3/Modul3_final/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Modul3_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Вас зовут {name} и вам {age}");
+
+            Console.Write("Введите вашу дату рождения: ");
+            string birthday = Console.ReadLine();
+            Console.WriteLine($"Ваша дата рождения {birthday}");
+        }
+    }
+}
